adaa779f1ebff30b428fce16b1b896a5e3c86329
AJ <dev@andrew24.com>
2025-02-05 09:41:40 -0500

    fix: change card query

--- lib/quickbooks/service/card.rb
+++ lib/quickbooks/service/card.rb
@@ -3,10 +3,20 @@
 module Quickbooks
   module Service
     class Card < BaseServiceJSON
-      def query(object_query = nil, options = {}, params = {}, headers = {})
-        headers['request-Id'] ||= generate_uniq_request_id
+      REST_RESOURCE = 'cards'
 
-        fetch_collection(object_query, model, options, params, headers)
+      # def query(object_query = nil, options = {}, params = {}, headers = {})
+      #   headers['request-Id'] ||= generate_uniq_request_id
+      #
+      #   fetch_collection(object_query, model, options, params, headers)
+      # end
+      def query(customer_id, request_id = nil)
+        url = url_for_resource(model::REST_RESOURCE)
+        response = do_http_get(url, {}, { 'request-Id' => request_id || generate_uniq_request_id })
+
+        return unless response&.code.to_i == 200
+
+        response
       end
 
       private
